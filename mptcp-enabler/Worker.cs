921dce71aa456e8985928dc135af0aeceae04bf3
Bilal Brarou <bilal.brarou@student.uclouvain.be>
2024-02-29 23:44:32 +0100

    awaker stopped on stop() method

--- mptcp-enabler/Worker.cs
+++ mptcp-enabler/Worker.cs
@@ -9,74 +9,24 @@ public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
     private const int RefreshDelay = 5000;
+    private MPTCPEnabler mptcpEnabler;
+    private WSLAwaker awaker;
     public Worker(ILogger<Worker> logger)
     {
         _logger = logger;
     }
-
-    private void WaitForServiceInWSL2()
-    {
-        var process = new Process
-        {
-            StartInfo = new ProcessStartInfo
-            {
-                FileName = "bash.exe",
-                Arguments = "-c 'ls'",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true,// Run as administrator
-            }
-        };
-
-        process.OutputDataReceived += (sender, e) =>
-        {
-            if (e.Data != null)
-            {
-                Console.WriteLine(e.Data);
-                _logger.LogInformation($"Output received: {e.Data}");
-            }
-        };
-
-        process.ErrorDataReceived += (sender, e) =>
-        {
-            if (e.Data != null)
-            {
-                _logger.LogError($"Error received: {e.Data}");
-            }
-        };
-
-        process.Exited += (sender, e) =>
-        {
-            _logger.LogInformation($"Process exited with code: {process.ExitCode}");
-        };
-
-        try
-        {
-            process.Start();
-            process.BeginOutputReadLine();
-            process.BeginErrorReadLine();
-            _logger.LogInformation($"Current user: {Environment.UserName}");
-            process.WaitForExit();
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError($"Exception: {ex.Message}");
-        }
-
-    }
-
+    
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         NetworkConfig config = NetworkConfig.LoadConfigFromFile();
         WslConfigManager wslConfigManager = new WslConfigManager(config);
-        var mptcpEnabler = new MPTCPEnabler(_logger, stoppingToken, RefreshDelay, config);
-        WSLAwaker awaker = new WSLAwaker(config);
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            await Task.Delay(RefreshDelay, stoppingToken);
-        }
-        awaker.Stop();
+        mptcpEnabler = new MPTCPEnabler(_logger, stoppingToken, RefreshDelay, config);
+        awaker = new WSLAwaker(config);
     }
-    
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        awaker.Stop();
+        return base.StopAsync(cancellationToken);
+    }
 }
